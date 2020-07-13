d13bc0aa985f39985418dfa73e235b6a59c6d5e1
Quoc Tuan <39939107+quoctuanck99@users.noreply.github.com>
2020-07-13 15:31:57 +0700

    initial

---
+++ ISCProject/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISCProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        [Route("login")]
+        public IActionResult Index()
+        {
+            return View("Views/LoginAndReg/Login.cshtml");
+        }
+    }
+}
---
+++ ISCProject/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISCProject.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("user/personalPage")]
+        public IActionResult Index()
+        {
+            return View("Views/Home/UserPage.cshtml");
+        }
+    }
+}
