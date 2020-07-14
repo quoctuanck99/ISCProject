2bfc7841deb081eb1a7aa0d247a696ab6ae1c245
thang1160 <35557579+thang1160@users.noreply.github.com>
2020-07-14 15:25:54 +0700

    login

---
+++ ISCProject_API/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ISCProject_API.Models;
+using ISCProject_Models;
+
+namespace ISCProject_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly FotozyContext _context;
+
+        public LoginController(FotozyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Account>> CheckLogin(string username, string password)
+        {
+            return await _context.Account.Where(x => x.AccountName == username && x.HashedPassword == password).FirstOrDefaultAsync();
+        }
+    }
+}
